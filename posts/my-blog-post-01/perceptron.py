423958e447e2f23469fc7e5345f2a95f5edbad33
Xianzhi_Wang <xianzhiwang2023@gmail.com>
2023-02-16 20:26:31 -0500

    commit

---
+++ posts/my-blog-post-01/perceptron.py
@@ -0,0 +1,15 @@
+
+
+class Perceptron:
+    def __init__(self, w, history):
+        self.w = w
+        self.history = history
+
+    def accuracy(X,y,w,b):
+        pass
+    
+    def fit():
+        pass
+
+    def myprint(self):
+        print("it's running around!")
