f79ee8052cb73d2cce94bfca6d5c469e323e376e
Xianzhi_Wang <xianzhiwang2023@gmail.com>
2023-02-20 22:21:26 -0500

    commit on feb 20 2023

--- posts/my-blog-post-01/.ipynb_checkpoints/perceptron-checkpoint.py
+++ posts/my-blog-post-01/.ipynb_checkpoints/perceptron-checkpoint.py
@@ -1,2 +1,78 @@
-def my_custom_function():
-    print("I implemented this function in the file " + __name__ + ".py")
+import typing
+from typing import TypeVar
+import random
+import numpy as np
+
+# def char_func(self,z):
+#     return 1.0 if (z<0) else 0.0
+
+class Perceptron:
+    def __init__(self, history, maxiter):
+        self.history = history
+        self.maxiter = maxiter
+
+    def accuracy(self, X: np.array, y: np.array, w: np.array, ) -> float:
+        return ( (X @ w) * y >0).mean()
+
+    
+    def fit(self, X: np.array, w: np.array, y: np.array, b: int, maxiter: int) -> None:
+        m,p = X.shape
+        index = 0
+        X_ = np.append(X, np.ones((X.shape[0],1)),1)
+        w_ = np.append(w, -b) 
+        y_ = 2 * y-1
+        while (index <= maxiter) and (self.accuracy(X = X_ ,y = y_ ,w = w_ ) < 1):
+            # initialize a random initial weight vector 
+
+            # pick a random index i \in [n]
+            i = random.randint(np.ndim(x_i))
+            x_i = X_[i,:]
+            y_i = y_[i]
+
+            
+            # compute 
+            dotproduct = y_i *np.dot(w_,x_i)
+            w_next = w_ + (dotproduct<0) * y_i * x_i
+            
+            # update before next loop
+            w_ = w_next
+            index += 1 
+
+
+    
+   
+
+
+    def predict(self, X):
+        pass
+        
+    def score(X,y):
+        pass
+
+    def myprint(self):
+        print("it's running around!")
+
+        
+    # 1*(x>0)
+    # 1*True = 1
+    # 1*False = 0
+    # if some_bool:
+    #     w += update
+    #     w += some_bool*update
+    # done = (steps==maxiter) or (accuracy = 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- posts/my-blog-post-01/perceptron.py
+++ posts/my-blog-post-01/perceptron.py
@@ -7,33 +7,44 @@ import numpy as np
 #     return 1.0 if (z<0) else 0.0
 
 class Perceptron:
-    def __init__(self, w, history,maxiter):
-        self.w = w
+    def __init__(self, history, maxiter):
         self.history = history
         self.maxiter = maxiter
+        self.w = None    
 
-    def accuracy(X: np.array, y: np.array, w: np.array, b: np.array) -> int:
-        return ( (X@w-b) * y >0).mean()
+    def accuracy(self, X: np.array, y: np.array, w: np.array, ) -> float:
+        return ( (X @ w) * y >0).mean()
 
     
-    def fit(self, X: np.array, y: np.array, b: int, maxiter: int) -> None:
+    def fit(self, X: np.array, w: np.array, y: np.array, b: int, maxiter: int) -> None:
         m,p = X.shape
         index = 0
         X_ = np.append(X, np.ones((X.shape[0],1)),1)
-        w_ = np.append(self.w, -b) 
-        while index <= maxiter and accuracy(X,y,w_,b) :
-            a = random.randint(np.ndim(w))
-            dotproduct = self.y_*np.dot(w_,X_)
-            w_next = w_ + (dotproduct<0)*self.y*self.X
+        w_ = np.append(w, -b) 
+        y_ = 2 * y-1
+        while (index <= maxiter) and (self.accuracy(X = X_ ,y = y_ ,w = w_ ) < 1):
+            # initialize a random initial weight vector 
+
+            # pick a random index i \in [n]
+            foo = np.shape(w)
+            i = np.random.randint(foo)
+            x_i = X_[i,:][0]
+            y_i = y_[i]
+            # print(x_i)
+
+            
+            # compute 
+            dotproduct = y_i *np.dot(w_,x_i)
+            w_next = w_ + (dotproduct<0) * y_i * x_i
+            
+            # update before next loop
+            w_ = w_next
             index += 1 
-
-
-
-        # fitting and training
-        m,n = self.X.shape
-        
+        self.w = w_
+        print(self.accuracy(X = X_ ,y = y_ ,w = w_ ))
     
-    
+
+
     
    
 
@@ -45,7 +56,7 @@ class Perceptron:
         pass
 
     def myprint(self):
-        print("it's running around!")
+        print("it's working! OMG!")
 
         
     # 1*(x>0)
