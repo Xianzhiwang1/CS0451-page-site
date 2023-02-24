3c455ea2463ccd5b42874c86efe553b7e7029ca8
Xianzhi_Wang <xianzhiwang2023@gmail.com>
2023-02-24 16:26:59 -0500

    commit Feb 24

---
+++ posts/my-blog-post-02-gradient-descent/solutions_logistic.py
@@ -0,0 +1,49 @@
+
+class LogisticRegression():
+    def __init__(self) -> None:
+        self.w_ = None
+        self.loss_history = []
+        self.score_history = []
+
+
+
+    def fit(self, X,y) -> None:
+        pass
+
+    def loss(self, X,y):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
