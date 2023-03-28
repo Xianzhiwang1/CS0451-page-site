2f7c48d7da09f0ab941312649612d863c10ff88c
Xianzhi_Wang <xianzhiwang2023@gmail.com>
2023-03-27 22:04:46 -0400

    commit

--- posts/my-blog-post-04-linear-regress/linear_regression.py
+++ posts/my-blog-post-04-linear-regress/linear_regression.py
@@ -25,6 +25,7 @@ class LinearRegression:
         self.L = Lasso(alpha = 0.001)
         self.lasso_score_history = []
         self.fit_gradient_score_history = []
+        self.fit_analytic_score_history = []
 
 
 
@@ -45,11 +46,16 @@ class LinearRegression:
     def gradient(self, P, q):
         return  P@self.w - q 
     
-    def fit_gradient(self, X_: np.array,  y: np.array, alpha = 0.00001, max_epochs = 1e4) -> None:
+    def fit_gradient(self, X_: np.array,  y: np.array, alpha = 0.00001, max_epochs = 1e2) -> None:
         # self.w = np.array([[0.99],
         #                   [0.99]])
         features = X_.shape[1]
-        self.w = np.random.rand(features)
+        # print(f"this is features: {features}")
+        # self.w = np.random.rand(features, 1)
+        self.w = np.random.rand(features,1)
+        # print(f"this is self.w {self.w}")
+        # print(f"this is self.w shape {self.w.shape}")
+
         self.score_history.append(self.score(X_,y))
         # initialization
         prev_loss = self.Big_L(X_ = X_, y = y) + 0.99 
@@ -57,6 +63,7 @@ class LinearRegression:
         i = 0
         P = X_.T@X_
         q = X_.T@y
+        # print(f" this is self.gradient {self.gradient(P,q)}")
 
         # while loop iteration
         while (not done) and (i <= max_epochs): 
@@ -72,6 +79,27 @@ class LinearRegression:
             self.loss_history.append(self.new_loss)
             self.score_history.append(self.score(X_,y))
             i += 1
+
+    # def fit_grad(self, X_: np.array,  y: np.array, alpha = 0.00001, max_epochs = 50) -> None:
+    #     features = X_.shape[1]
+    #     self.w = np.random.randn(features,1)
+    #     # initialization
+    #     prev_loss = self.Big_L(X_ = X_, y = y) + 0.99 
+    #     done = False
+    #     i = 0
+    #     P = X_.T@X_
+    #     q = X_.T@y
+    #     # while loop iteration
+    #     while (not done) and (i <= max_epochs): 
+    #         self.w = self.w - 2 * alpha * self.gradient(P, q) # gradient step
+    #         self.new_loss = self.Big_L(X_ = X_, y = y) # compute loss
+
+    #         # check if loss hasn't changed and terminate if so
+    #         if np.isclose(self.new_loss, prev_loss, rtol=1e-05, atol=1e-08):          
+    #             done = True
+            
+    #         prev_loss = self.new_loss
+    #         i += 1
 
 
     def fit_analytic(self, X_: np.array, y: np.array) -> None:
@@ -120,6 +148,18 @@ class LinearRegression:
         
         return X_train, y_train, X_val, y_val
 
+    def data(self, n_train = 100, n_val = 100, p_features = 1, noise = .1, w = None):
+        if w is None: 
+            w = np.random.rand(p_features + 1) + .2
+            # print(w)
+        
+        X_train = np.random.rand(n_train, p_features)
+        y_train = self.pad(X_train)@w + noise*np.random.randn(n_train,1)
+
+        X_val = np.random.rand(n_val, p_features)
+        y_val = self.pad(X_val)@w + noise*np.random.randn(n_val,1)
+        
+        return X_train, y_train, X_val, y_val
 
 # n_train = 100
 # n_val = 100
@@ -127,15 +167,26 @@ class LinearRegression:
 # noise = 0.2
 
     def lasso_score(self, n_train: int, n_val: int, noise: float) -> None:
-        for p_features in range(1, n_train+42):
-            X_train, y_train, X_val, y_val = self.LR_data(n_train, n_val, p_features, noise)
+        for p_features in range(1, n_train+2):
+            X_train, y_train, X_val, y_val = self.data(n_train, n_val, p_features, noise)
+            # X_train = self.pad(X_train)
             self.L.fit(X_train, y_train)
             self.lasso_score_history.append(self.L.score(X_val, y_val))
 
     def lin_regress_score(self, n_train: int, n_val: int, noise: float) -> None:
-        for p_features in range(1, n_train+42):
-            X_train, y_train, X_val, y_val = self.LR_data(n_train, n_val, p_features, noise)
+        for p_features in range(1, n_train+2):
+            X_train, y_train, X_val, y_val = self.data(n_train, n_val, p_features, noise)
+            # X_train = self.pad(X_train)
             self.fit_gradient(X_train, y_train)
+
             self.fit_gradient_score_history.append(self.score(X_val, y_val))
+
+    def lin_regress_score_analytic(self, n_train: int, n_val: int, noise: float) -> None:
+        for p_features in range(1, n_train+2):
+            X_train, y_train, X_val, y_val = self.data(n_train, n_val, p_features, noise)
+            # X_train = self.pad(X_train)
+            self.fit_analytic(X_train, y_train)
+
+            self.fit_analytic_score_history.append(self.score(X_val, y_val))
 
         
