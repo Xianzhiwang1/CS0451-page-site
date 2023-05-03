3e029105c9a415bd8879bacc3a5f1e60ba11bea3
Xianzhi Wang <wilsonxavier057@gmail.com>
2023-05-02 22:43:25 -0400

    update

--- posts/my-blog-post-04-linear-regress/linear_regression.py
+++ posts/my-blog-post-04-linear-regress/linear_regression.py
@@ -47,14 +47,8 @@ class LinearRegression:
         return  P@self.w - q 
     
     def fit_gradient(self, X_: np.array,  y: np.array, alpha: float, max_epochs: int) -> None:
-        # self.w = np.array([[0.99],
-        #                   [0.99]])
         features = X_.shape[1]
-        # print(f"this is features: {features}")
-        # self.w = np.random.rand(features, 1)
         self.w = np.random.rand(features,1)
-        # print(f"this is self.w {self.w}")
-        # print(f"this is self.w shape {self.w.shape}")
 
         self.score_history.append(self.score(X_,y))
         # initialization
@@ -80,26 +74,26 @@ class LinearRegression:
             self.score_history.append(self.score(X_,y))
             i += 1
 
-    # def fit_grad(self, X_: np.array,  y: np.array, alpha = 0.00001, max_epochs = 50) -> None:
-    #     features = X_.shape[1]
-    #     self.w = np.random.randn(features,1)
-    #     # initialization
-    #     prev_loss = self.Big_L(X_ = X_, y = y) + 0.99 
-    #     done = False
-    #     i = 0
-    #     P = X_.T@X_
-    #     q = X_.T@y
-    #     # while loop iteration
-    #     while (not done) and (i <= max_epochs): 
-    #         self.w = self.w - 2 * alpha * self.gradient(P, q) # gradient step
-    #         self.new_loss = self.Big_L(X_ = X_, y = y) # compute loss
+    def fit_grad(self, X_: np.array,  y: np.array, alpha = 0.00001, max_epochs = 50) -> None:
+        features = X_.shape[1]
+        self.w = np.random.randn(features,1)
+        # initialization
+        prev_loss = self.Big_L(X_ = X_, y = y) + 0.99 
+        done = False
+        i = 0
+        P = X_.T@X_
+        q = X_.T@y
+        # while loop iteration
+        while (not done) and (i <= max_epochs): 
+            self.w = self.w - 2 * alpha * self.gradient(P, q) # gradient step
+            self.new_loss = self.Big_L(X_ = X_, y = y) # compute loss
 
-    #         # check if loss hasn't changed and terminate if so
-    #         if np.isclose(self.new_loss, prev_loss, rtol=1e-05, atol=1e-08):          
-    #             done = True
+            # check if loss hasn't changed and terminate if so
+            if np.isclose(self.new_loss, prev_loss, rtol=1e-05, atol=1e-08):          
+                done = True
             
-    #         prev_loss = self.new_loss
-    #         i += 1
+            prev_loss = self.new_loss
+            i += 1
 
 
     def fit_analytic(self, X_: np.array, y: np.array) -> None:
@@ -112,14 +106,9 @@ class LinearRegression:
     def predict(self, X_) -> np.array:
         return X_ @ self.w
 
-
-
-
     
     def accuracy(self, X: np.array, y: np.array, w: np.array, ) -> float:
         pass
-
-
 
     def score(self, X_, y) -> float:
         n_of_points = X_.shape[0]
@@ -127,17 +116,12 @@ class LinearRegression:
         y_bar = y.mean()
         quotient = ((y_hat - y)**2).sum() / ((y_bar - y)**2).sum()
         result = (1- quotient)/n_of_points
-
         return result 
 
 
     def pad(self, X):    
         return np.append(X, np.ones((X.shape[0],1)), 1)
 
-    def myprint(self):
-        print("it's working!")
-
-        
 
     def data(self, n_train = 100, n_val = 100, p_features = 1, noise = .1, w = None):
         if w is None: 
@@ -168,7 +152,7 @@ class LinearRegression:
         for p_features in range(1, n_train+2):
             X_train, y_train, X_val, y_val = self.data(n_train, n_val, p_features, noise)
             # X_train = self.pad(X_train)
-            self.fit_gradient(X_train, y_train)
+            self.fit_grad(X_train, y_train)
 
             self.fit_gradient_score_history.append(self.score(X_val, y_val))
 
